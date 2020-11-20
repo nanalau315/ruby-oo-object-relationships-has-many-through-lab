2bbc611662013c176e28c38d25565bfd42803c3a
Nana Lau <nanalau315@hotmail.com>
2020-11-19 22:44:43 -0500

    commit tree 6136014af2da9c38d607a4e297ab59d168546a74

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,1 +1,21 @@
+class Patient
+    attr_accessor :name
+    @@all = []
 
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def new_appointment(doctor, date)
+        Appointment.new(date, self, doctor)
+    end
+
+    def appointments
+        Appointment.all.select {|app| app.patient == self}
+    end
+
+    def doctors
+        appointments.map {|app|app.doctor}
+    end
+end
