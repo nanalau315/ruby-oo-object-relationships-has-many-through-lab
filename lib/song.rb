7aff8cd2638a737b109de4187badfb7945f5749f
Nana Lau <nanalau315@hotmail.com>
2020-11-19 22:49:38 -0500

    Completed lab

--- lib/song.rb
+++ lib/song.rb
@@ -1,1 +1,17 @@
+class Song
+    attr_accessor :name
+    attr_reader :artist, :genre #why is artist and genre in reader?
+    @@all = []
 
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+end
